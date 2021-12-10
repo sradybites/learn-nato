76af9f873146ebbd151812b020f38ac9e0b56044
Brady Alexander Sites <bas339@cornell.edu>
2021-12-10 15:05:11 -0500

    updated mechanics
    - game is now functional with entire NATO alphabet
    - keywords "score" and "done" added

--- consts.h
+++ consts.h
@@ -9,6 +9,35 @@ namespace NATOLib
     const std::string A = "Alfa";
     const std::string B = "Bravo";
     const std::string C = "Charlie";
+
+    const std::map<char, std::string> dict = {
+       {'A', "Alfa"},
+       {'B', "Bravo"},
+       {'C', "Charlie"},
+       {'D', "Delta"},
+       {'E', "Echo"},
+       {'F', "Foxtrot"},
+       {'G', "Golf"},
+       {'H', "Hotel"},
+       {'I', "India"},
+       {'J', "Juliet"},
+       {'K', "Kilo"},
+       {'L', "Lima"},
+       {'M', "Mike"},
+       {'N', "November"},
+       {'O', "Oscar"},
+       {'P', "Papa"},
+       {'Q', "Quebec"},
+       {'R', "Romeo"},
+       {'S', "Sierra"},
+       {'T', "Tango"},
+       {'U', "Uniform"},
+       {'V', "Victor"},
+       {'W', "Whiskey"},
+       {'X', "X-ray"},
+       {'Y', "Yankee"},
+       {'Z', "Zulu"}
+    };
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -8,13 +8,41 @@ using namespace std;
 int main()
 {
     string input;
-    char letter = 'A';
+    bool correct;
+    int score = 0;
+    int tot = 0;
+    //char letter = 'A';
+    // uppercase ASCII alphabet start at 65
+    while (input != "done") {
+      int letter_val = rand() % 26 + 65;
+      char letter = (char)letter_val;
 
-    cout << letter;
-    cout << "\nType something: ";
-    cin >> input;
-    string message = (input == NATOLib::A) ? "Good job!" : "Try again";
-    cout << message;
+      cout << letter << '\n';
+      tot = tot + 1;
+      cin >> input;
+
+      while (!correct) {
+        try {
+          correct = input == NATOLib::dict.at(letter);
+          if (input == "done") {
+            cout << "Goodbye.\n";
+            break;
+          } else if (input == "score") {
+            cout << "Score: " << score << "/" << tot;
+            break;
+          } else if (correct) {
+            cout << "Good job!\n";
+            score = score + 1;
+          } else {
+            throw 0;
+          }
+        } catch (...) {
+          cout << "Try again.\n";
+          cin >> input;
+        }
+      }
+      correct = false;
+    }
 
     return 0;
 }
